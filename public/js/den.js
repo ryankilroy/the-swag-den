6d4b87ce406f589cb910bc2333744a0ee1e9f009
Ryan Collins <rcollins@pivotal.io>
2016-09-30 14:53:07 -0400

    RyanMC - Countdown blink, formatting, and mobile font size

--- public/js/den.js
+++ public/js/den.js
@@ -1,6 +1,7 @@
 function run(n) {
 	var num = Number(n%5);
 	var title = document.getElementById("title");
+	var strokeWidth = "0.25vw";
 	//console.log(title);
 	if (title){
 		//console.log(num);
@@ -15,12 +16,14 @@ function run(n) {
 				title.style.color = 'green';
 				break;
 			case 3:
-				title.style.color = 'gold';
+				title.style.color = 'yellow';
 				break;
 			case 4:
-				title.style.color = 'white';
+				title.style.color = 'transparent';
+				strokeWidth = "0px";
 				break;
 		}
+		title.style.webkitTextStrokeWidth = strokeWidth;
 	}
 	setTimeout(function(){
 		//console.log(num+1);
@@ -35,13 +38,28 @@ function countdown() {
 		var endtime = 'October 01 2016 20:30:00 GMT-0400';
 		var time = getTimeRemaining(endtime);
 
-		var hours = document.getElementsByClassName("hours");
-		var minutes = document.getElementsByClassName("minutes");
-		var seconds = document.getElementsByClassName("seconds");
+		var hours = document.getElementsByClassName("hours")[0];
+		var minutes = document.getElementsByClassName("minutes")[0];
+		var seconds = document.getElementsByClassName("seconds")[0];
 
-		hours[0].innerHTML = time.hours;
-		minutes[0].innerHTML = time.minutes;
-		seconds[0].innerHTML = time.seconds;
+		if (time.hours < 1 && hours.className.indexOf("blink") == -1){
+			hours.className += " blink";
+			minutes.className += " blink";
+			seconds.className += " blink";
+		}
+		hours.innerHTML = time.hours + time.days*24;
+		if (time.hours + time.days*24 < 10){
+			hours.innerHTML = '0' + hours.innerHTML;
+		}
+		minutes.innerHTML = time.minutes;
+		if (time.minutes < 10){
+			minutes.innerHTML = '0' + minutes.innerHTML;
+		}
+
+		seconds.innerHTML = time.seconds;
+		if (time.seconds < 10){
+			seconds.innerHTML = '0' + seconds.innerHTML;
+		}
 		//hours[1].innerHTML = time.hours;
 		//minutes[1].innerHTML = time.minutes;
 		//seconds[1].innerHTML = time.seconds;
